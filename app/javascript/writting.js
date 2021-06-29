19638ff5ab5cbbc3ea6fa03a2ca300db48cf7867
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-29 10:19:59 +0900

    ビュー作成

---
+++ app/javascript/writting.js
@@ -0,0 +1,12 @@
+$(function () {
+    const CLASSNAME = "-visible";
+    const TIMEOUT = 1500;
+    const $target = $(".title");
+
+    setInterval(() => {
+      $target.addClass(CLASSNAME);
+      setTimeout(() => {
+        $target.removeClass(CLASSNAME);
+      }, TIMEOUT);
+    }, TIMEOUT * 3)
+  });
