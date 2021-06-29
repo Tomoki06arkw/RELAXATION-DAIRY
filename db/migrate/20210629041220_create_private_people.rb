1617090b47cad0f3641f5782b4ea4d3e4eb8ff0d
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-29 19:53:41 +0900

    ログイン機能実装完了

--- db/migrate/20210629041220_create_private_people.rb
+++ db/migrate/20210629041220_create_private_people.rb
@@ -2,7 +2,7 @@ class CreatePrivatePeople < ActiveRecord::Migration[6.0]
   def change
     create_table :private_people do |t|
       t.string      :nickname,              null: false
-      t.integer     :address_id,            null: false
+      t.string      :address,               null: false
       t.text        :profile,               null: false
       t.references  :user,                  foregin_key: true
       t.timestamps
