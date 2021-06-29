77bbf953645eac28b905f7b7618165a0381fe457
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-29 12:11:01 +0900

    Textモデル作成

---
+++ db/migrate/20210629024558_create_texts.rb
@@ -0,0 +1,9 @@
+class CreateTexts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :texts do |t|
+      t.text :writing,             null: false
+      t.references :user,          foregin_key: true
+      t.timestamps
+    end
+  end
+end
