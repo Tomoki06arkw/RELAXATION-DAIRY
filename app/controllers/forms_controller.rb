96e687b727261cf987d9800adce4f67caccdcaa0
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-07-14 11:05:01 +0900

    条件分岐とセキュリティー強化

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,6 +1,7 @@
 class FormsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :set_form, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :new, :create]
 
   def index
     @forms = Form.all
@@ -51,4 +52,10 @@ class FormsController < ApplicationController
     params.require(:form).permit(:title, :content, :start_time, :image).merge(user_id: current_user.id)
   end
   
+  def move_to_index
+    unless user_signed_in? && current_user.id == @form.user.id
+      redirect_to action: :index
+    end
+  end
+
 end
